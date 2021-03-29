f57343479236ecfd937b5b7c8677d7fc4f937183
Szpakusik <szpakusik@gmail.com>
2021-03-29 16:12:51 +0200

    Logger fix

--- todo-service/src/middlewares/logger.ts
+++ todo-service/src/middlewares/logger.ts
@@ -9,7 +9,7 @@ const setupLogger = (app: Express) => {
       +d.getHours().toString().padStart(2, '0')+":"
       +d.getMinutes().toString().padStart(2, '0')+":"
       +d.getSeconds().toString().padStart(2, '0');
-    console.log('\x1b[34m',"/"+req.method, 'Date:', dateString, "from", req.headers.origin, "\x1b")
+    console.log('\x1b[34m',"/"+req.method, 'Date:', dateString, "from", req.headers.origin)
     next()
   })
 }
